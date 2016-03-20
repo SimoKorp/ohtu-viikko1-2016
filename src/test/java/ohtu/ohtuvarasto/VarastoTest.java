cb9c53ce2b5edf16837c1b7cbd9d842d8564ac3e
Simo <simokorpikoski@gmail.com>
2016-03-20 21:41:26 +0200

    testi muutettu toimimattomaksi

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -27,7 +27,7 @@ public class VarastoTest {
 
     @Test
     public void uudellaVarastollaOikeaTilavuus() {
-        assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus-4);
+        assertEquals(10, varasto.getTilavuus()-4, vertailuTarkkuus);
     }
 
     @Test
